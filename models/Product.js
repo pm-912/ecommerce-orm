027544442aec42b022ad378e6d3bcad705f56b98
Peetah <peterleemartin@gmail.com>
2024-02-05 23:04:56 -0600

    added some functionality to get /api/products

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -15,8 +15,9 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-      model: 'category',
-      key: 'id'}
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,21 +2,23 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     product_id: {
       type: DataTypes.INTEGER,
       references: {
-      model: 'product',
-      key: 'id',}
+        model: 'product',
+        key: 'id',
+      }
     },
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
-      model: 'tag',
-      key: 'id',}
+        model: 'tag',
+        key: 'id',
+      }
     },
   },
   {
